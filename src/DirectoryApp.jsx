45a74b929e2ffc2c1beed5e14357d1e68228af60
0xfreddy <freddy0x@proton.me>
2024-12-20 20:59:56 +0400

    Update DirectoryApp.jsx

--- src/DirectoryApp.jsx
+++ src/DirectoryApp.jsx
@@ -957,11 +957,11 @@ return (
         }`}
       >
         <LayoutGrid size={16} />
-        Directory
+Directory
       </button>
     </div> 
   </div>
-</div>
+  )
   
   <div className="w-full mx-auto p-6 space-y-8 md:space-y-0 flex-grow flex flex-col md:grid md:grid-cols-2 md:space-x-8">
   <div className={`rounded-lg md:shadow-xl w-full ${activeView !== "canvas" ? "hidden md:block" : ""}`}>
