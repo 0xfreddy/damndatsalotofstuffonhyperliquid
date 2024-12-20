63228081c0f97d3618bb8b6024451292f296b61e
0xfreddy <freddy0x@proton.me>
2024-12-20 21:02:43 +0400

    Update DirectoryApp.jsx

--- src/DirectoryApp.jsx
+++ src/DirectoryApp.jsx
@@ -962,7 +962,7 @@ Directory
     </div> 
   </div>
   )
-  
+  return(
   <div className="w-full mx-auto p-6 space-y-8 md:space-y-0 flex-grow flex flex-col md:grid md:grid-cols-2 md:space-x-8">
   <div className={`rounded-lg md:shadow-xl w-full ${activeView !== "canvas" ? "hidden md:block" : ""}`}>
     <canvas
